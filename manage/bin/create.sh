8e4e96e475c04ce3423056fefb5c121e23f4b92f
azsgit <10238943+azsgit@users.noreply.github.com>
2026-05-06 16:07:47 +0900

    wip

---
+++ manage/bin/create.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pthtop="$(cd "$(dirname "${0}")/../../../.." && pwd)"
+source "${pthtop}"/manage/lib/params.sh
+source "${pthtop}"/manage/lib/shared.sh
+source "${pthcrr}"/params.sh
+
+pthapp="${pthsrc}"/appmsh
+namsrv='xoxxox_sttmsh'
+lngtgt='ja'
+
+addimg ${imgtgt} "${cnfimg}" "${pthdoc}"
+test -d "${pthapp}" || mkdir "${pthapp}"
+if cd "${pthapp}"
+then
+  test -d msh || mkdir msh
+  test -d msh/download.moonshine.ai || \
+  docker compose -f "${cnfcmp}" run --rm ${namsrv} /env/python/bin/python3 -m moonshine_voice.download --language ${lngtgt}
+fi
---
+++ manage/bin/params.sh
@@ -0,0 +1,4 @@
+pthdoc="${pthtop}"/export/cnf/xoxxox/docenv_envmsh
+cnfimg="${pthtop}"/export/cnf/xoxxox/docenv_envmsh.txt
+cnfcmp="${pthtop}"/export/cnf/xoxxox/docker_sttmsh.yml
+imgtgt='xoxxox/envmsh:001'
