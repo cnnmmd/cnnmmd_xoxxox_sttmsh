b4d582642e9aa5c94626b31b710de90f5a882e78
azsgit <10238943+azsgit@users.noreply.github.com>
2026-08-08 21:50:01 +0900

    Fix options in model download script

--- manage/bin/create.sh
+++ manage/bin/create.sh
@@ -15,5 +15,5 @@ if cd "${pthapp}"
 then
   test -d msh || mkdir msh
   test -d msh/download.moonshine.ai || \
-  docker compose -f "${cnfcmp}" run --rm ${namsrv} /env/python/bin/python3 -m moonshine_voice.download --language ${lngtgt}
+  docker compose -f "${cnfcmp}" run --rm ${namsrv} /env/python/bin/python3 -m moonshine_voice.download --stt --language ${lngtgt}
 fi
